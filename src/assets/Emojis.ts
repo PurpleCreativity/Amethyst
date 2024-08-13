e38bc0c8b3c7452f989c73d185616f19eef0d132
Purple_Creativity <thepurplecreativity@gmail.com>
2024-08-13 19:13:23 +0200

    Emojis

--- src/assets/Emojis.ts
+++ src/assets/Emojis.ts
@@ -1,3 +1,8 @@
 export default {
-    
+    "thinking": "<a:thinking:1272876208668938313>",
+    "loading": "<a:loading:1272878146802155531>",
+
+    "rover": "<:RoVer:1272943872820904039>",
+    "bloxlink": "<:BloxLink:1272943880492290170>",
+    "roblox": "<:Roblox:1272943859999178864>"
 }
