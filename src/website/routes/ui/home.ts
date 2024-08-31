b74e5c8ac9bf80f4a0ef7a4647f5932162dc8a0b
Purple_Creativity <thepurplecreativity@gmail.com>
2024-08-31 15:34:30 +0200

    Protect /home

--- src/website/routes/ui/home.ts
+++ src/website/routes/ui/home.ts
@@ -1,10 +1,14 @@
 import Route from "../../../classes/Route.js";
 import path from "node:path"
+import isAuthenticated from "../../middlewares/isAuthenticated.js";
 
 const route = new Route({
     path: "home",
-    public: true,
     method: "GET",
+
+    public: true,
+    middleware: [isAuthenticated],
+
     execute(req, res) {
         res.status(200).sendFile(path.join(process.cwd(), "src/website/html/home.html"));
     }
