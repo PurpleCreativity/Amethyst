15fa31b03fbaee75b40ce5f52b5b2d1553f5324b
Purple_Creativity <thepurplecreativity@gmail.com>
2024-09-02 15:25:59 +0200

    Make emojis on the main bot

--- src/assets/Emojis.ts
+++ src/assets/Emojis.ts
@@ -1,21 +1,20 @@
 import client from "../index.js";
 
 export default {
-    "thinking": "<a:thinking:1272876208668938313>",
-    "loading": "<a:loading:1272878146802155531>",
+    "thinking": "<a:thinking:1280155082146578514>",
     
-    "rover": "<:RoVer:1272943872820904039>",
-    "bloxlink": "<:BloxLink:1272943880492290170>",
-    "roblox": "<:Roblox:1272943859999178864>",
+    "rover": "<:RoVer:1280155243191205971>",
+    "bloxlink": "<:BloxLink:1280155259687276546>",
+    "roblox": "<:Roblox:1280155154749849673>",
     
-    "warning": "<:warning:1275515330990702633>",
+    "warning": "<:warning:1280156078000308355>",
     
-    "delete": "<:delete:1276153837958201356>",
-    "import": "<:import:1276154186186227822>",
-    "upload": "<:upload:1276154508996771931>",
-    "folder_open": "<:folder_open:1276154608636526645>",
-    "add": "<:add:1276155767174725703>",
-    "description": "<:description:1276156265155792916>",
-    "check": "<:check:1276156510807785483>",
-    "permissions": "<:permissions:1277019889466347633>"
+    "delete": "<:delete:1280155041054982216>",
+    "import": "<:import:1280154991025459260>",
+    "upload": "<:upload:1280154960985853982>",
+    "folder_open": "<:folder_open:1280154816114589749>",
+    "add": "<:add:1280154783579373642>",
+    "description": "<:description:1280154907189444628>",
+    "check": "<:check:1280154582131146762>",
+    "permissions": "<:permissions:1280154866655821858>"
 }
