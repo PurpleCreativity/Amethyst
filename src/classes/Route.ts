862696b9a61078e777eb4faa698523b40646ab02
Purple_Creativity <thepurplecreativity@gmail.com>
2024-09-03 15:08:26 +0200

    Update error handling on routes to use the new formatting

--- src/classes/Route.ts
+++ src/classes/Route.ts
@@ -52,27 +52,27 @@ class Route {
                 }
 
 				const APIKey = req.headers["x-api-key"] || req.headers["X-Api-Key"] || req.headers.authorization || req.headers.Authorization;
-				if (!APIKey) return res.status(401).send({ error: "No API Key provided", message: "Provide a API key header (x-api-key or X-Api-Key or authorization or Authorization) with your request" }).end();
+				if (!APIKey) return res.status(401).send({ error: "No API Key provided", message: `Provide a API key header ("x-api-key" or "X-Api-Key" or "authorization" or "Authorization") with your request` }).end();
 
 				const guildAndkey = await client.API.GetGuildProfileFromAPIKey(APIKey as string);
 				const guildProfile = guildAndkey?.guildProfile;
 				const keyData = guildAndkey?.keyData;
 
-				if (!guildProfile || !keyData) return res.status(401).send({ error: "Invalid API Key", message: "The API key provided is invalid" }).end();
+				if (!guildProfile || !keyData) return res.status(401).send({ error: { name: "Invalid API Key", message: "The API key provided is invalid" } }).end();
 
-				if (guildProfile.API.banned) return res.status(401).send({ error: "API Banned", message: "The API is banned" }).end();
-				if (!guildProfile.API.enabled) return res.status(401).send({ error: "API Disabled", message: "The API is disabled" }).end();
-				if (!keyData.enabled) return res.status(401).send({ error: "API Key Disabled", message: "The API key provided is disabled" }).end();
+				if (guildProfile.API.banned) return res.status(401).send({ error: { name: "API Banned", message: "Your guild has been API banned, contact the developer for more information" } }).end();
+				if (!guildProfile.API.enabled) return res.status(401).send({ error: { name: "API Disabled", message: "Your guild has disabled all API functionability" } }).end();
+				if (!keyData.enabled) return res.status(401).send({ error: { name: "Key disabled", message: "The provided API Key is disabled" } }).end();
 				if (keyData.permissions.includes("Administrator")) return this.execute(req, res, guildProfile);
 
 				if (this.permissions.length > 0) {
 					const hasPermission = this.permissions.some(permission => keyData.permissions.includes(permission));
-					if (!hasPermission) return res.status(403).send({ error: "Missing Permissions", message: "You do not have the required permissions to access this route" }).end();
+					if (!hasPermission) return res.status(403).send({ error: { name: "Insufficient Permissions", message: "The provided API Key does not have sufficient permissions to access this route" } }).end();
 				}
 
                 this.execute(req, res, guildProfile);
             } catch (error) {
-                return res.status(500).send({ error: "An error occured while processing your request.", message: error }).end();
+                return res.status(500).send({ error: { name: "Internal Server Error", message: error } }).end();
             }
         }
     }
