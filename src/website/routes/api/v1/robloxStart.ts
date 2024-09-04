380c3ac2fbcbf4ea9def452ee8cdca25377a41dd
Purple_Creativity <thepurplecreativity@gmail.com>
2024-09-04 22:04:36 +0200

    mr team made me unforget to add api/v1/roblox/experiences/start

---
+++ src/website/routes/api/v1/robloxStart.ts
@@ -0,0 +1,21 @@
+import Route from "../../../../classes/Route.js";
+
+export default new Route({
+    path: "roblox/experiences/start",
+    method: "GET",
+
+    public: true,
+
+    execute: async (req, res) => {
+        const placeId = req.query.placeId;
+        const gameInstanceId = req.query.gameInstanceId;
+
+        if (!placeId) return res.status(400).json({ error: { name: "Missing placeId query parameter", message: "Input a placeId query parameter" } });
+
+        if (gameInstanceId) {
+            res.status(200).redirect(`roblox://experiences/start?placeId=${placeId}&gameInstanceId=${gameInstanceId}`);
+        } else {
+            res.status(200).redirect(`roblox://experiences/start?placeId=${placeId}`)
+        }
+    }
+})
