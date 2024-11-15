8ad42c26b20108eca74c19bdbb234343675e1c79
Purple_Creativity <thepurplecreativity@gmail.com>
2024-11-15 18:36:42 +0100

    save changes

--- src/core/Database.ts
+++ src/core/Database.ts
@@ -7,15 +7,29 @@
 import mongoose from "mongoose";
 
 import type Client from "../classes/Client.ts";
+import type { guildProfileInterface } from "../schemas/guildProfile.ts";
+import type { userProfileInterface } from "../schemas/userProfile.ts";
 
 export default class Database {
     client: Client;
+
+    cache = {
+        guilds: new Map<string, guildProfileInterface>(),
+        users: new Map<string, userProfileInterface>(),
+    };
 
     constructor(client: Client) {
         this.client = client;
     }
 
     Init = async () => {
-        console.log(await mongoose.connect(this.client.config.credentials.databaseURI));
+        try {
+            await mongoose.connect(this.client.config.credentials.databaseURI);
+        } catch (error) {
+            this.client.error("Failed to connect to database");
+            this.client.error(error);
+        }
+
+        if (mongoose.connection.readyState === 1) this.client.success("Connected to Database");
     };
 }
