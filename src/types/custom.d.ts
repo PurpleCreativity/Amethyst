487654f49b9ef8aee8a583758c068541ed06305a
Purple_Creativity <thepurplecreativity@gmail.com>
2024-11-16 19:41:27 +0100

    NumberRange

---
+++ src/types/custom.d.ts
@@ -0,0 +1,3 @@
+type NumberRange<N extends number, M extends number> = number extends N | M
+    ? never
+    : Exclude<{ [K in N | M]: K }[N | M], number>;
--- src/types/main.d.ts
+++ src/types/main.d.ts
@@ -1,2 +1,4 @@
-/// <reference path="./config.d.ts" />
 /// <reference path="./global.d.ts" />
+/// <reference path="./custom.d.ts" />
+
+/// <reference path="./config.d.ts" />
