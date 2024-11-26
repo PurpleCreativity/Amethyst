7c5979188e195bd19af0115e3aa676fed90e291f
Purple_Creativity <thepurplecreativity@gmail.com>
2024-11-26 22:51:39 +0100

    t

--- src/types/main.d.ts
+++ src/types/main.d.ts
@@ -1,6 +1,6 @@
-/// <reference path="./global.d.ts" />
-/// <reference path="./custom.d.ts" />
+/// <reference path="global.d.ts" />
+/// <reference path="custom.d.ts" />
 
-/// <reference path="./config.d.ts" />
+/// <reference path="config.d.ts" />
 
-/// <reference path="./Functions.d.ts" />
+/// <reference path="Database.d.ts" />
