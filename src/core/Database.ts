ddc468882c2adfaa6cde9946320c60c9d9795a8f
Purple_Creativity <thepurplecreativity@gmail.com>
2024-11-27 22:40:08 +0100

    database work

--- src/core/Database.ts
+++ src/core/Database.ts
@@ -36,25 +36,166 @@ export default class Database {
         }
     };
 
+    //! TODO: modify some values later
+    //! maybe port into sql files instead of string code
     private initializeTables = async () => {
+        const stringContentLength = 512;
+
         const connection = await this.getConnection();
 
         try {
             await connection.query(
-                `CREATE TABLE IF NOT EXISTS UserProfile (
-                    IV VARCHAR(255) NOT NULL UNIQUE,
+                `CREATE TABLE IF NOT EXISTS user_profiles (
+                    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 
-                    Id VARCHAR(255) PRIMARY KEY,
-                    Name VARCHAR(255) NOT NULL,
+                    iv VARBINARY(16) NOT NULL,
 
-                    RobloxId BIGINT DEFAULT NULL,
-                    RobloxUsername VARCHAR(255) DEFAULT NULL,
-                    RobloxUpdatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                    discord_id VARCHAR(20) NOT NULL UNIQUE,
+                    discord_username VARCHAR(32) NOT NULL,
 
-                    FFlags JSON DEFAULT '{}',
-                    Settings JSON DEFAULT '{}',
+                    roblox_id BIGINT UNSIGNED UNIQUE,
+                    roblox_username VARCHAR(20),
 
-                    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                    settings JSON NOT NULL DEFAULT '{}',
+                    fflags JSON NOT NULL DEFAULT '{}',
+
+                    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                );`,
+            );
+
+            await connection.query(
+                `CREATE TABLE IF NOT EXISTS guild_profiles (
+                    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+
+                    iv VARBINARY(16) NOT NULL,
+                    shortname VARCHAR(10) NOT NULL UNIQUE,
+
+                    guild_id VARCHAR(20) NOT NULL,
+                    guild_name VARCHAR(100) NOT NULL,
+
+                    roblox_group_id VARCHAR(20),
+
+                    api_rover_key VARCHAR(100),
+                    api_bloxlink_key VARCHAR(100),
+                    api_enabled BOOLEAN NOT NULL DEFAULT FALSE,
+
+                    settings JSON NOT NULL DEFAULT '{}',
+                    fflags JSON NOT NULL DEFAULT '{}',
+
+                    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                );`,
+            );
+
+            await connection.query(
+                `CREATE TABLE IF NOT EXISTS guild_users (
+                    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                    guild_profile_id BIGINT UNSIGNED NOT NULL,
+                    user_profile_id BIGINT UNSIGNED NOT NULL,
+
+                    points INT NOT NULL,
+
+                    note_content VARCHAR(${stringContentLength}),
+                    note_visible BOOLEAN NOT NULL DEFAULT TRUE,
+                    note_updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+                    ranklock_rank INT UNSIGNED NOT NULL DEFAULT 0,
+                    ranklock_shadow BOOLEAN NOT NULL DEFAULT FALSE,
+                    ranklock_reason VARCHAR(${stringContentLength}),
+                    ranklock_updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+                    FOREIGN KEY (guild_profile_id) REFERENCES guild_profiles(id),
+                    FOREIGN KEY (user_profile_id) REFERENCES user_profiles(id)
+                );`,
+            );
+
+            await connection.query(
+                `CREATE TABLE IF NOT EXISTS roblox_places (
+                    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                    guild_profile_id BIGINT UNSIGNED NOT NULL,
+
+                    place_nickname VARCHAR(32) NOT NULL,
+                    place_id VARCHAR(20) NOT NULL,
+                    place_key VARCHAR(1000) NOT NULL,
+
+                    FOREIGN KEY (guild_profile_id) REFERENCES guild_profiles(id)
+                );`,
+            );
+
+            await connection.query(
+                `CREATE TABLE IF NOT EXISTS api_keys (
+                    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                    guild_profile_id BIGINT UNSIGNED NOT NULL,
+
+                    key_name VARCHAR(20) NOT NULL,
+                    key_value VARCHAR(100) NOT NULL,
+                    enabled BOOLEAN NOT NULL DEFAULT TRUE,
+                    permissions JSON NOT NULL DEFAULT '{}',
+
+                    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                    created_by BIGINT UNSIGNED NOT NULL,
+
+                    FOREIGN KEY (guild_profile_id) REFERENCES guild_profiles(id),
+                    FOREIGN KEY (created_by) REFERENCES user_profiles(id)
+                );`,
+            );
+
+            await connection.query(
+                `CREATE TABLE IF NOT EXISTS point_logs (
+                    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                    guild_profile_id BIGINT UNSIGNED NOT NULL,
+
+                    data JSON NOT NULL,
+                    notes VARCHAR(${stringContentLength}),
+
+                    creator_name VARCHAR(100) NOT NULL,
+                    creator_id VARCHAR(20) NOT NULL,
+                    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+                    FOREIGN KEY (guild_profile_id) REFERENCES guild_profiles(id)
+                );`,
+            );
+
+            await connection.query(
+                `CREATE TABLE IF NOT EXISTS schedule_types (
+                    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                    guild_profile_id BIGINT UNSIGNED NOT NULL,
+                    name VARCHAR(32) NOT NULL,
+
+                    icon VARCHAR(1024) NOT NULL,
+                    color VARCHAR(20) NOT NULL,
+                    description VARCHAR(${stringContentLength}),
+
+                    use_roblox_schedule BOOLEAN NOT NULL DEFAULT FALSE,
+                    use_discord_schedule BOOLEAN NOT NULL DEFAULT FALSE,
+
+                    can_schedule_roles JSON NOT NULL DEFAULT '{}',
+                    can_schedule_users JSON NOT NULL DEFAULT '{}',
+
+                    FOREIGN KEY (guild_profile_id) REFERENCES guild_profiles(id)
+                );`,
+            );
+
+            await connection.query(
+                `CREATE TABLE IF NOT EXISTS schedule_events (
+                    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                    guild_profile_id BIGINT UNSIGNED NOT NULL,
+
+                    type VARCHAR(100) NOT NULL,
+                    notes VARCHAR(${stringContentLength}),
+                    place_id BIGINT UNSIGNED,
+
+                    time BIGINT NOT NULL,
+                    duration INT NOT NULL,
+
+                    host_name VARCHAR(100) NOT NULL,
+                    host_id BIGINT UNSIGNED NOT NULL,
+
+                    ongoing BOOLEAN NOT NULL DEFAULT FALSE,
+
+                    discord_event_id VARCHAR(32) NOT NULL,
+                    roblox_event_id VARCHAR(32) NOT NULL,
+
+                    FOREIGN KEY (guild_profile_id) REFERENCES guild_profiles(id)
                 );`,
             );
 
@@ -64,35 +205,6 @@ export default class Database {
             this.client.error(error);
 
             process.exit(1);
-        }
-
-        await connection.end();
-    };
-
-    private addUser = async (user: User) => {
-        const connection = await this.getConnection();
-
-        try {
-            return await connection.query(
-                `
-                INSERT INTO UserProfile (
-                    iv, user_id, user_name, 
-                    roblox_user_id, roblox_user_name,
-                    roblox_updated_at, FFlags, settings
-                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-                
-                ON DUPLICATE KEY UPDATE 
-                    user_name = VALUES(user_name),
-                    roblox_user_id = VALUES(roblox_user_id),
-                    roblox_user_name = VALUES(roblox_user_name),
-                    roblox_updated_at = VALUES(roblox_updated_at),
-                    FFlags = VALUES(FFlags),
-                    settings = VALUES(settings);
-                `,
-                [this.client.Functions.GenerateIV(), user.id, user.username, null, null, new Date(), "{}", "{}"],
-            );
-        } catch (error) {
-            this.client.error(error);
         }
     };
 
