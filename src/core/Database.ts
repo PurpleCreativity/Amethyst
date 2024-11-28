1d2dfe42979b1941fad603840c3e0d76dce2e953
Purple_Creativity <thepurplecreativity@gmail.com>
2024-11-28 22:16:56 +0100

    use the default values

--- src/core/Database.ts
+++ src/core/Database.ts
@@ -86,10 +86,9 @@ export default class Database {
             const insertQuery = await connection.query(
                 `INSERT INTO user_profiles (
                     iv, discord_id, discord_username,
-                    roblox_id, roblox_username,
-                    settings, fflags
-                ) VALUES (?, ?, ?, ?, ?, ?, ?);`,
-                [this.client.Functions.GenerateIV(), user.id, user.username, null, null, "{}", "{}"],
+                    roblox_id, roblox_username
+                ) VALUES (?, ?, ?, ?, ?);`,
+                [this.client.Functions.GenerateIV(), user.id, user.username, null, null],
             );
 
             await connection.commit();
