c73177969234cbb37077d42453a8a723feedc6ed
Purple_Creativity <thepurplecreativity@gmail.com>
2024-11-28 20:56:55 +0100

    i want to push for now

---
+++ src/classes/database/UserProfile.ts
@@ -0,0 +1,71 @@
+import client from "../../main.js";
+
+export type rawUserData = {
+    _id: number,
+    _v: number;
+
+    iv: Buffer,
+
+    discord_id: string,
+    discord_username: string,
+
+    roblox_id: number | null,
+    roblox_username: string | null,
+
+    settings: Record<string, unknown>,
+    fflags: Record<string, unknown>,
+
+    updated_at: Date,
+}
+
+export default class UserProfile {
+    readonly rawdata: rawUserData;
+
+    readonly _id: number;
+    readonly _v: number;
+
+    readonly iv: string;
+
+    readonly discord_id: string;
+    readonly discord_username: string;
+
+    readonly roblox_id: number | null;
+    readonly roblox_username: string | null;
+
+    readonly settings: Record<string, unknown>;
+    readonly fflags: Record<string, unknown>;
+
+    readonly updated_at: Date;
+
+    constructor(rawdata: rawUserData) {
+        this.rawdata = rawdata;
+
+        this._id = rawdata._id;
+        this._v = rawdata._v;
+
+        this.iv = rawdata.iv.toString("hex");
+
+        this.discord_id = rawdata.discord_id;
+        this.discord_username = rawdata.discord_username;
+
+        this.roblox_id = rawdata.roblox_id;
+        this.roblox_username = rawdata.roblox_username;
+
+        this.settings = rawdata.settings;
+        this.fflags = rawdata.fflags;
+
+        this.updated_at = rawdata.updated_at;
+    };
+
+    getSetting = (key: string): unknown => {
+        return this.settings[key];
+    }
+
+    setSetting = (key: string, value: unknown): void => {
+        this.settings[key] = value;
+    };
+
+    save = async (): Promise<void> => {
+        
+    };
+}
--- src/core/Database.ts
+++ src/core/Database.ts
@@ -2,15 +2,30 @@ import fs from "node:fs";
 import path from "node:path";
 import mariadb, { SqlError } from "mariadb";
 import type Client from "../classes/Client.ts";
+import { type Snowflake, User } from "discord.js";
+import UserProfile from "../classes/database/UserProfile.js";
 
 export default class Database {
     client: Client;
     private pool: mariadb.Pool;
+    readonly loadingOrder = [
+        "user_profiles",
+        "guild_profiles",
+
+        "roblox_places",
+        "api_keys",
+        "point_logs",
+
+        "schedule_types",
+        "schedule_events",
+    ];
 
     constructor(client: Client) {
         this.client = client;
         this.pool = mariadb.createPool(client.config.credentials.database);
     }
+
+    //? Internal Methods
 
     getConnection = async (): Promise<mariadb.Connection> => {
         try {
@@ -38,38 +53,27 @@ export default class Database {
     };
 
     private initializeTables = async () => {
-        const filesPath = path.join(process.cwd(), "sql/tables");
+        const filesPath = path.join(process.cwd(), "sql");
         const connection = await this.getConnection();
 
-        const loadingOrder = [
-            "user_profiles",
-            "guild_profiles",
-
-            "roblox_places",
-            "api_keys",
-            "point_logs",
-
-            "schedule_types",
-            "schedule_events",
-        ];
-
         try {
-            for (const tableName of loadingOrder) {
+            for (const tableName of this.loadingOrder) {
                 const filePath = path.join(filesPath, `${tableName}.sql`);
 
                 if (fs.existsSync(filePath)) {
                     const sql = fs.readFileSync(filePath, "utf-8");
 
                     await connection.query(sql);
+
                     this.client.verbose(`Initialized table: ${tableName}`);
                 } else {
-                    this.client.warn(`SQL file for table '${tableName}' not found: ${filePath}`);
+                    // this.client.warn(`SQL file for table '${tableName}' not found: ${filePath}`);
                 }
             }
 
             this.client.success("Initialized Database tables");
         } catch (error) {
-            this.client.error("There was an error initialising the Database tables:");
+            this.client.error("There was an error initializing the Database tables:");
             this.client.error(error);
 
             process.exit(1);
@@ -78,7 +82,65 @@ export default class Database {
         }
     };
 
+    //? Data handling
+
+    private addUserProfile = async (user: User | string | Snowflake) => {
+        if (typeof user === "string") user = (await this.client.Functions.fetchUser(user)) as User;
+        if (!user || !(user instanceof User)) throw new Error("Unknown user.");
+
+        const connection = await this.getConnection();
+
+        try {
+            const insertQuery = await connection.query(
+                `INSERT INTO user_profiles (
+                    iv, discord_id, discord_username,
+                    roblox_id, roblox_username,
+                    settings, fflags
+                ) VALUES (?, ?, ?, ?, ?, ?, ?);`,
+                [this.client.Functions.GenerateIV(), user.id, user.username, null, null, "{}", "{}"],
+            );
+
+            return insertQuery;
+        } catch (error) {
+            this.client.error(`Failed to add user [${user.username}:${user.id}] to the database:`);
+            this.client.error(error);
+        } finally {
+            await connection.end();
+        }
+    };
+
+    getUserProfile = async (userId: string) => {
+        const connection = await this.getConnection();
+        try {
+            const existingUser = await connection.query("SELECT * FROM user_profiles WHERE discord_id = ?", [userId]);
+            if (existingUser.length > 0) {
+                const rawdata = existingUser[0];
+
+                return new UserProfile(rawdata);
+            }
+
+            const profile_id = (await this.addUserProfile(userId)).insertId;
+            const rawdata = (await connection.query("SELECT * FROM user_profiles WHERE _id = ?", [profile_id]))[0];
+
+            return new UserProfile(rawdata);
+        } catch (error) {
+            this.client.error(`Failed to get user [${userId}] to the database:`);
+            this.client.error(error);
+        } finally {
+            await connection.end();
+        }
+    };
+
     Init = async () => {
-        await this.initializeTables();
+        if (this.client.devMode) await this.initializeTables();
+
+        /*
+        const profile = await this.getUserProfile("762329291169857537");
+        if (!profile) return;
+        console.log(profile);
+
+        profile.setSetting("hello", 1);
+        console.log(profile.getSetting("hello"));
+        */
     };
 }
