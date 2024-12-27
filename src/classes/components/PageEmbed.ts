99d1bc1d6174b87187c17700d6bd294966539e7a
Purple_Creativity <thepurplecreativity@gmail.com>
2024-12-27 15:33:16 +0100

    Search button

--- src/classes/components/PageEmbed.ts
+++ src/classes/components/PageEmbed.ts
@@ -1,8 +1,17 @@
-import { type APIEmbedField, type ButtonInteraction, ButtonStyle, EmbedBuilder } from "discord.js";
+import {
+    type APIEmbedField,
+    type ButtonInteraction,
+    ButtonStyle,
+    EmbedBuilder,
+    TextInputBuilder,
+    TextInputStyle,
+} from "discord.js";
 import Emojis from "../../../public/Emojis.json" with { type: "json" };
 import Button from "./Button.js";
 import ButtonEmbed from "./ButtonEmbed.js";
 import Embed, { type EmbedOptions } from "./Embed.js";
+import Modal from "../prompts/Modal.js";
+import client from "../../main.js";
 
 export type PageEmbedOptions = {
     baseEmbed: Embed;
@@ -22,6 +31,7 @@ export default class PageEmbed extends ButtonEmbed {
     backButton: Button;
 
     firstPageButton: Button;
+    searchPageButton: Button;
     lastPageButton: Button;
 
     constructor(opts: PageEmbedOptions) {
@@ -81,6 +91,116 @@ export default class PageEmbed extends ButtonEmbed {
                 allowedUsers: opts.allowedUsers,
                 function: async (interaction) => {
                     await this.toPage(interaction, 1);
+                },
+            }),
+        );
+
+        this.searchPageButton = this.addButton(
+            new Button({
+                label: "Search",
+                emoji: Emojis.search,
+                style: ButtonStyle.Secondary,
+                allowedUsers: opts.allowedUsers,
+                function: async (interaction) => {
+                    const modal = new Modal({
+                        title: "Search",
+                        inputs: [
+                            new TextInputBuilder()
+                                .setCustomId("field_name")
+                                .setLabel("A field's name")
+                                .setMaxLength(256)
+                                .setStyle(TextInputStyle.Short)
+                                .setRequired(false),
+
+                            new TextInputBuilder()
+                                .setCustomId("field_value")
+                                .setLabel("A field's value")
+                                .setMaxLength(1024)
+                                .setStyle(TextInputStyle.Paragraph)
+                                .setRequired(false),
+                        ],
+                    });
+
+                    const response = await modal.prompt(interaction);
+                    await response.deferReply();
+
+                    const lookName = response.fields.getTextInputValue("field_name");
+                    const lookValue = response.fields.getTextInputValue("field_value");
+
+                    if (lookName.length === 0 && lookValue.length === 0) {
+                        return await response.editReply({
+                            embeds: [
+                                client.Functions.makeErrorEmbed({
+                                    title: "An error occured while searching",
+                                    description: "You must either pass trough a `field_name` or `field_value`!",
+                                }),
+                            ],
+                        });
+                    }
+
+                    // forEach is the devil
+                    for (let i = 0; i < this.embeds.length; i++) {
+                        const embed = this.embeds[i];
+
+                        const fields = embed.data.fields ?? [];
+                        for (let j = 0; j < fields.length; j++) {
+                            const field = fields[j];
+
+                            if (
+                                (lookName && field.name.includes(lookName)) ||
+                                (lookValue && field.value.includes(lookValue))
+                            ) {
+                                const buttonEmbed = new ButtonEmbed(
+                                    client.Functions.makeInfoEmbed({
+                                        title: "Field found",
+                                        description: `The field was found on **Page ${i + 1}**, Field #${j + 1}.`,
+                                    }),
+                                );
+
+                                buttonEmbed.addButton(
+                                    new Button({
+                                        label: "Jump to page",
+                                        style: ButtonStyle.Primary,
+                                        allowedUsers: opts.allowedUsers,
+                                        function: async (buttonInteraction) => {
+                                            await this.toPage(buttonInteraction, i + 1);
+                                            await response.deleteReply();
+                                        },
+                                    }),
+                                );
+
+                                buttonEmbed.addButton(
+                                    new Button({
+                                        label: "View field",
+                                        style: ButtonStyle.Secondary,
+                                        allowedUsers: opts.allowedUsers,
+                                        function: async (buttonInteraction) => {
+                                            await buttonInteraction.reply({
+                                                embeds: [
+                                                    client.Functions.makeInfoEmbed({
+                                                        title: field.name,
+                                                        description: field.value,
+                                                    }),
+                                                ],
+                                                ephemeral: true,
+                                            });
+                                        },
+                                    }),
+                                );
+
+                                return await response.editReply(buttonEmbed.getMessageData());
+                            }
+                        }
+                    }
+
+                    await response.editReply({
+                        embeds: [
+                            client.Functions.makeErrorEmbed({
+                                title: "No Results",
+                                description: "No fields matching your search criteria were found.",
+                            }),
+                        ],
+                    });
                 },
             }),
         );
